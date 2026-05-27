3be412cae8a452f44cc2697b03cc5149bf6f35c5
camartinezrincon <ca.martinezrincon@unicienciabga.edu.co>
2026-05-26 21:34:05 -0500

    Update RecurringExpensesScreen.kt

--- app/src/main/java/com/example/control_gastos/ui/recurring_expenses/RecurringExpensesScreen.kt
+++ app/src/main/java/com/example/control_gastos/ui/recurring_expenses/RecurringExpensesScreen.kt
@@ -1,6 +1,7 @@
 package com.example.control_gastos.ui.recurring_expenses
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
